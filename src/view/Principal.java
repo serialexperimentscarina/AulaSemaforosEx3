344d7eae24e2fe7a5bf2d4fa496609edd6b91091
serialexperimentscarina <108838850+serialexperimentscarina@users.noreply.github.com>
2023-03-12 23:35:20 -0300

    feat: Principal

--- src/view/Principal.java
+++ src/view/Principal.java
@@ -1,9 +1,20 @@
 package view;
 
+import java.util.concurrent.Semaphore;
+
+import controller.ThreadEscuderia;
+
 public class Principal {
 
 	public static void main(String[] args) {
-
+		Semaphore semaforo = new Semaphore(5);
+		
+		System.out.println(" == A corrida começou !! == ");
+		
+		for (int i = 0; i < 7; i++) {
+			Thread Escuderia = new ThreadEscuderia(i, semaforo);
+			Escuderia.start();
+		}
 	}
 
 }
